49e4428f850809a1d40a1916806d742e071303f4
Smyrechynskyi <41690469+Smyrechynskyi@users.noreply.github.com>
2021-11-18 19:05:02 +0200

    Initial + Task 1

---
+++ 01.multithreading/MultiThreading.Task1.100Tasks/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * 1.	Write a program, which creates an array of 100 Tasks, runs them and waits all of them are not finished.
+ * Each Task should iterate from 1 to 1000 and print into the console the following string:
+ * “Task #0 – {iteration number}”.
+ */
+using System;
+using System.Threading.Tasks;
+
+namespace MultiThreading.Task1._100Tasks
+{
+    class Program
+    {
+        const int TaskAmount = 100;
+        const int MaxIterationsCount = 1000;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".Net Mentoring Program. Multi threading V1.");
+            Console.WriteLine("1.	Write a program, which creates an array of 100 Tasks, runs them and waits all of them are not finished.");
+            Console.WriteLine("Each Task should iterate from 1 to 1000 and print into the console the following string:");
+            Console.WriteLine("“Task #0 – {iteration number}”.");
+            Console.WriteLine();
+            
+            HundredTasks();
+
+            Console.ReadLine();
+        }
+
+        static void HundredTasks()
+        {
+            var tasks = new Task[TaskAmount];
+
+            for (var i = 0; i < TaskAmount; i++)
+            {
+                tasks[i] = new Task(PrintIterations);
+            }
+
+            foreach (var task in tasks)
+            {
+                task.Start();
+            }
+
+            Task.WaitAll(tasks);
+        }
+
+        private static void PrintIterations()
+        {
+            for (var i = 0; i < MaxIterationsCount; i++)
+            {
+                Output(Task.CurrentId.Value, i + 1);
+            }
+        }
+
+        static void Output(int taskNumber, int iterationNumber)
+        {
+            Console.WriteLine($"Task #{taskNumber} – {iterationNumber}");
+        }
+    }
+}
